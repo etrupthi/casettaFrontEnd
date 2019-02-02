ae54fbc7991784dc57006aa4ac1b37f753031a45
AkhilaV <>
2019-02-02 13:18:59 +0530

    React App working 2 feb 2018

---
+++ src/components/App/index.js
@@ -0,0 +1,104 @@
+import React from "react";
+import HotelList from "./hlist/index";
+import ViewHotel from "./viewhotel/index";
+import NavBar from "./navbar/index";
+import {
+   BrowserRouter as Router,
+   Route,
+   Switch,
+   Redirect,
+} from 'react-router-dom'
+//import styled from "styled-components";
+// const Title = styled.h1`
+//   font-size: 1.5em;
+//   text-align: center;
+//   color: palevioletred;
+// `;
+
+
+// const tableHeaders = ['Id','Name','Location','Price'];
+
+class App extends React.Component{
+   constructor(props){
+      super(props)
+       this.state = {
+       data:[]
+      }
+   }
+
+      componentDidMount(){
+
+         const url = "http://localhost:9000/hotels"; 
+         let headers = new Headers();
+     
+         headers.append('Content-Type', 'application/json');
+         headers.append('Accept', 'application/json');
+     
+         headers.append('Access-Control-Allow-Origin', url);
+         headers.append('Access-Control-Allow-Credentials', 'true');
+     
+         headers.append('GET', 'POST');
+     
+         fetch(url, {
+             headers: headers,
+             method: 'GET'
+         })
+         .then(response => response.json())
+         .then(contents => {console.log("in fetch: "+ contents);
+                             this.setState ({
+                             data : contents})
+              })
+         .catch(() => console.log("Can’t access " + url + " response. "))
+        
+     }
+
+   render(){
+      return(
+         <Router>
+            <Switch>
+               <Route exact path="/home" render={(props) => {
+                  return(
+                     <div>
+                        <NavBar/>
+                        <HotelList 
+                        hotel={this.state.data}
+                        history={props.history}/>
+                     </div>
+                  )
+               }}/>
+               <Route exact path="/viewhotel/:id"  render={(props) => {
+                  const hid = props.match.params.id
+                  const hdata=this.state.data[hid-1]
+                  var arr = [];
+                  for (var key in hdata) {
+                     arr.push(hdata[key]);
+                  }
+                  console.log(arr)
+                  const newRecord = {
+                     name: arr[1],
+                     location: arr[2],
+                     price: arr[3]
+                  }
+            
+                  return(
+                     <div>
+                        <NavBar/>
+                        <ViewHotel
+                           name  = {newRecord.name} 
+                           location = {newRecord.location} 
+                           price  = {newRecord.price}
+                           history={props.history}
+                         />
+                     </div>
+                  )
+               }}/>
+               <Redirect to="/home"/>
+            </Switch> 
+         </Router>
+      );
+   }
+}
+
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+
+import App from "./components/App";
+
+const render = Component => ReactDOM.render(
+    <App/>,
+    document.getElementById("root")
+);
+
+render(App);
